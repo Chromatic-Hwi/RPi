35a4ace8f81ff7838d4c25340d24449c9ce1560f
RPi-Hwi <Chromatic_365@naver.com>
2022-05-25 23:05:27 +0900

    쉘 스크립트 작동 과정 파악을 위한 커밋

---
+++ only_push.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+$(sudo git push origin main)
+
+expect "Username for 'https://github.com': "
+sleep 0.1
+send "Chromatic-Hwi\r")
+sleep 0.1
+send "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")
