600e4fef9d2a232fa39d2042a79980961508b910
RPi-Hwi <Chromatic_365@naver.com>
2022-05-25 22:31:08 +0900

    쉘 스크립트 작동 과정 파악을 위한 커밋

--- git_push.sh
+++ git_push.sh
@@ -4,11 +4,11 @@ echo "Start GitHub Push"
 echo "Enter the commit message: "
 read CM
 
-$(sudo git config --global user.name "RPi-Hwi")
-$(sudo git config --global user.emial "Chromatic_365@naver.com")
+#$(sudo git config --global user.name "RPi-Hwi")
+#$(sudo git config --global user.emial "Chromatic_365@naver.com")
 
 $(sudo git add /home/hwi/문서/Git)
-$(sudo git commit -m echo $(CM))
+$(sudo git commit -m echo $CM)
 $(sudo git branch -M main)
 
 $(sudo git remote add add origin https://github.com/Chromatic-Hwi/RPi.git)
